52dd40002b6531546918f72b647b99e041ad940b
KimTam <kimtam486@gmail.com>
2026-03-17 14:33:39 +0700

    updated: change techstack

---
+++ backend/src/main/java/com/krisaleth/scriptify/ScriptifyApplication.java
@@ -0,0 +1,13 @@
+package com.krisaleth.scriptify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptifyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/krisaleth/scriptify/ScriptifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krisaleth.scriptify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
