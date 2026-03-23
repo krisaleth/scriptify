b64766b834cdcd7e34117676d917deac3ebe8547
KimTam <kimtam486@gmail.com>
2026-03-23 11:46:09 +0700

    chore: change context-path

--- backend/src/main/java/com/krisaleth/scriptify/controller/AdminSingerController.java
+++ backend/src/main/java/com/krisaleth/scriptify/controller/AdminSingerController.java
@@ -6,7 +6,6 @@ import com.krisaleth.scriptify.service.SingerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
